3a4d4db1656a080d12d32aae6c1f894ea5196186
Pablo Dinella <pablo.dinella@rivendel.com.br>
2018-04-27 14:18:41 -0300

    firestore import flat chats list

--- data/src/addToFirestore.js
+++ data/src/addToFirestore.js
@@ -1,7 +1,9 @@
 const firebase = require("firebase");
 require("firebase/firestore"); // Required for side-effects
+const slug = require('slug');
 const escapeString = require('escape-string-regexp');
-const data = require("../output/entries-normalized.json");
+// const data = require("../output/entries-normalized.json");
+const data = require("../output/entries.json");
 
 firebase.initializeApp({
   apiKey: "AIzaSyAi1KlX6q2P3Be3M1gvwt-fLlwg0G7e53A",
@@ -14,6 +16,8 @@ firebase.initializeApp({
 
 var db = firebase.firestore();
 
+addFlatten()
+
 function encode(decoded) {
   return encodeURIComponent(decoded).replace(/\./g, '%2E')
 }
@@ -22,37 +26,56 @@ function decode(encoded) {
   return decodeURIComponent(encoded.replace('%2E', '.'))
 }
 
-// db.collection('tags').get()
-//   .then(snapshot => {
-//       snapshot.forEach(doc => {
-//           // console.log(doc.id, '=>', doc.data());
-//           // console.log(doc.collection('tags').get().then(sn => { console.log(sn.length);}));
-//           console.log(db.collection('tags').doc(doc.id).collection('chats').get().then(sn => { console.log(doc.id, '=>', sn.size); }));
-//       });
-//   })
-//   .catch(err => {
-//       console.log('Error getting documents', err);
-//   });
-
-
-data && Object.keys(data).forEach((key, i) => {
-  // if (i > 1) {
-  //   return
-  // }
-
-  const nestedContent = data[key];
-
-  if (typeof nestedContent === "object") {
-    Object.keys(nestedContent).forEach(docTitle => {
-      const tagRef = db.collection('tags').doc(encode(key))
-      tagRef.set({
-        label: key
-      })
-      tagRef.collection('chats').doc(encode(docTitle)).set(nestedContent[docTitle]).then((res) => {
-        console.log("Document successfully written!");
-      }).catch((error) => {
-        console.error("Error writing document: ", error);
-      });
+function printByTags() {
+  db
+    .collection('tags')
+    .get()
+    .then(snapshot => {
+        snapshot.forEach(doc => {
+            // console.log(doc.id, '=>', doc.data());
+            // console.log(doc.collection('tags').get().then(sn => { console.log(sn.length);}));
+            console.log(db.collection('tags').doc(doc.id).collection('chats').get().then(sn => { console.log(doc.id, '=>', sn.size); }));
+        });
+    })
+    .catch(err => {
+        console.log('Error getting documents', err);
     });
-  }
-});
+}
+
+
+function addNested() {
+  data && Object.keys(data).forEach((key, i) => {
+    // if (i > 1) {
+    //   return
+    // }
+
+    const nestedContent = data[key];
+
+    if (typeof nestedContent === "object") {
+      Object.keys(nestedContent).forEach(docTitle => {
+        const tagRef = db.collection('tags').doc(encode(key))
+        tagRef.set({
+          label: key
+        })
+        tagRef.collection('chats').doc(encode(docTitle)).set(nestedContent[docTitle]).then((res) => {
+          console.log("Document successfully written!");
+        }).catch((error) => {
+          console.error("Error writing document: ", error);
+        });
+      });
+    }
+  });
+}
+
+function addFlatten() {
+  data.group.forEach((chat, i) => {
+    if (i > 2) {
+      return
+    }
+    const ref = db.collection('chats').doc(slug(chat.title))
+    ref.set({
+      ...chat,
+      type: 'group'
+    })
+  });
+}
